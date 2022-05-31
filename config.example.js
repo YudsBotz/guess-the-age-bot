a0cf3591402598c5ba25300f1d2edf18a3cfe1d6
lebihkerendarianduwarholl <106031738+YudsBotz@users.noreply.github.com>
2022-06-01 02:38:35 +0700

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,6 +1,6 @@
 module.exports = {
-	token: "1234567890:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-	rounds: 5, //rounds amount
-	waitDelay: 12000, //1 round duration
+	token : "5570399961:AAGwakW9Yv7NZOpsDlMOyHsPWc6cItvXXcc" ,
+	putaran : 5 , //jumlah putaran
+	waitDelay : 12000 , // durasi putaran
 	timerSteps: 3, //timer indicators
 }
